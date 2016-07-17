2ea0a80ef415f3bd22196c3a567106e74ed977d5
AlaMote <sergunie@gmail.com>
2016-07-17 12:09:13 +0300

    Update connect_db.php

--- web/php/connect_db.php
+++ web/php/connect_db.php
@@ -1,17 +1,11 @@
 <?php
 
-    if (__DIR__ == "Z:\\home\\alamote.ru\\atm\\php") {
-        $host = "localhost";
-        $user = "root";
+    
+        $host = "";
+        $user = "";
         $password = "";
-        $db_name = "atm";
-    }
-    else {
-        $host = "us-cdbr-iron-east-03.cleardb.net";
-        $user = "bfcb66f2c460d7";
-        $password = "df4f0bec";
-        $db_name = "heroku_3b707b6bd40ec3f";
-    }
+        $db_name = "";
+    
 
     $sql = new mysqli($host, $user, $password, $db_name);
 
